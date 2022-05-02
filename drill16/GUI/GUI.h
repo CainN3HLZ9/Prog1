f5aa1c0b7fbb7f2dfca2b5fc6af98b019a60466d
CainN3HLZ9 <100156749+CainN3HLZ9@users.noreply.github.com>
2022-05-02 21:16:47 +0100

    Update GUI.h

--- drill16/GUI/GUI.h
+++ drill16/GUI/GUI.h
@@ -7,18 +7,18 @@
 namespace Graph_lib
 {
 
-	typedef void* Address;    // Address is a synonym for void*
-	typedef void(*Callback)(Address, Address);    // FLTK's required function type for all callbacks
+	typedef void* Address;    
+	typedef void(*Callback)(Address, Address);    
 
 	template<class W> W& reference_to(Address pw)
-		// treat an address as a reference to a W
+		
 	{
 		return *static_cast<W*>(pw);
 	}
 	class Widget
 	{
-		// Widget is a handle to an Fl_widget - it is *not* an Fl_widget
-		// We try to keep our interface classes at arm's length from FLTK
+		
+		
 	public:
 		Widget(Point xy, int w, int h, const string& s, Callback cb)
 			: loc(xy), width(w), height(h), label(s), do_it(cb)
@@ -38,10 +38,10 @@ namespace Graph_lib
 		virtual ~Widget() { }
 
 	protected:
-		Window* own;    // every Widget belongs to a Window
-		Fl_Widget* pw;  // connection to the FLTK Widget
+		Window* own;    
+		Fl_Widget* pw;  
 	private:
-		Widget& operator=(const Widget&); // don't copy Widgets
+		Widget& operator=(const Widget&); 
 		Widget(const Widget&);
 	};
 	struct Button : Widget
